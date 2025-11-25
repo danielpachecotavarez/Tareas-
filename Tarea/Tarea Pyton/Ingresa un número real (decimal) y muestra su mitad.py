92d4c29f23e63f1a0bdaaea8966fb72f2f3a205a
danielpachecotavarez <daniel.pacheco.tavarez@gmail.com>
2025-11-24 20:51:03 -0400

    Add files via upload

---
+++ Tarea/Tarea Pyton/Ingresa un número real (decimal) y muestra su mitad.py
@@ -0,0 +1,5 @@
+
+
+numero_real = float(input("Ingresa un número decimal: "))
+print("La mitad de tu número es:", numero_real / 2)
+
--- Tarea/Tarea Pyton/Pide la edad al usuario. Si es mayor o igual a 18 muestra 'Eres mayor de edad', sino 'Eres menor de edad.py
+++ Tarea/Tarea Pyton/Pide la edad al usuario. Si es mayor o igual a 18 muestra 'Eres mayor de edad', sino 'Eres menor de edad.py
@@ -1,5 +1,4 @@
 
-
 edad = int(input("Ingresa tu edad: "))
 
 if edad >= 18:
